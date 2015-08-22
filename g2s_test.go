659e53f517374563d6e28b761e16dfa262904082
Brian Hatfield <bmhatfield@gmail.com>
2015-08-22 10:21:01 -0400

    Update g2s for sub-millisecond precision

--- g2s_test.go
+++ g2s_test.go
@@ -18,7 +18,7 @@ func TestCounter(t *testing.T) {
 
 	s.Counter(1.0, "gorets", 1)
 
-	if expected, got := "gorets:1|c", b.String(); expected != got {
+	if expected, got := "gorets:1.000000|c", b.String(); expected != got {
 		t.Errorf("expected %q, got %q", expected, got)
 	}
 }
@@ -32,7 +32,7 @@ func TestTiming(t *testing.T) {
 
 	s.Timing(1.0, "glork", 320*time.Millisecond)
 
-	if expected, got := "glork:320|ms", b.String(); expected != got {
+	if expected, got := "glork:320.000000|ms", b.String(); expected != got {
 		t.Errorf("expected %q, got %q", expected, got)
 	}
 }
@@ -61,7 +61,7 @@ func TestMany(t *testing.T) {
 	s.Counter(1.0, "foo", 1, 2, 3)
 	s.Timing(1.0, "bar", 4*time.Millisecond, 5*time.Millisecond)
 
-	expected := "foo:1|c\nfoo:2|c\nfoo:3|cbar:4|ms\nbar:5|ms"
+	expected := "foo:1.000000|c\nfoo:2.000000|c\nfoo:3.000000|cbar:4.000000|ms\nbar:5.000000|ms"
 	got := b.String()
 	if expected != got {
 		t.Errorf("expected %q, got %q", expected, got)
